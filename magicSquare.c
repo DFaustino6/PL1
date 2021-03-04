d473fae43794c8633d85679fb6c2e9546d9ef03c
DFaustino6 <a61171@ualg.pt>
2021-03-04 20:53:13 +0000

    up

--- magicSquare.c
+++ magicSquare.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 #include <math.h>
 
 void ints_println_basic(const int *a, int n)
@@ -21,27 +23,70 @@ int read_numbers(FILE *fp,int *m){
     return result;
 }
 
-void to_magic_square(int **ms,int *m,int n){
-    int result = sqrt(n);
+void to_magic_square(int l,int **ms,int *m,int n){
 
     for (int i = 0; i < n; i++)
     {
-        int row = (int)(i/result); 
-        int col = (int)(i%result); 
-        printf("%d %d %d\n",row,col,m[4]);
+        int row = (int)(i/l); 
+        int col = (int)(i%l); 
         ms[row][col]=m[i];  
     }
     
 }   
 
 
-void test_magic_square(){
+int col_sum(int n,int **m,int c){
+    int result = 0;
+    for (int i = 0; i < n; i++)
+        result+=m[i][c];
+}
+
+int row_sum(int n, int *a){
+    int result = 0;
+    for (int i = 0; i < n; i++)
+        result+=a[i];
+}
+
+int DLR_sum(int n,int **m){
+    int result = 0;
+    for (int i = 0; i < n; i++)
+        result+=m[i][i];
+}
+
+int DRL_sum(int n,int **m){
+    int result = 0;
+    for (int i = 0; i < n; i++)
+        result+=m[i][n-(i+1)];   
+}
+
+bool check_magic_square(int n,int **m){
+    bool b=false;
+    int col_value = 0;
+    int row_value = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        int 
+        if()
+        
+    }
+    
+    return false;
+}
+
+void magic_square(){
     FILE *fp = fopen("ms.txt","r"); 
     int m[1000];
-    int ms[100][100];
     int n=0;
     n = read_numbers(fp,m);
-    to_magic_square(ms,m,n);
+    int l = sqrt(n);
+
+    int **ms = malloc(l*sizeof(int));
+
+    for (int i = 0; i < l; i++)
+        ms[i] = malloc(l*sizeof(int));
+    
+    to_magic_square(l,ms,m,n);
     ints_println_basic(m,n);
     
     fclose(fp);
@@ -50,6 +95,6 @@ void test_magic_square(){
 
 int main(int argc, char const *argv[])
 {
-    test_magic_square();
+    magic_square();
     return 0;
 }
