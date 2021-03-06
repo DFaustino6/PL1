4ead16c44ef72f0738c52427d5bf3d905a7c17c3
DFaustino6 <diogof99@outlook.pt>
2021-03-06 21:17:11 +0000

    up

--- magicSquare.c
+++ magicSquare.c
@@ -39,39 +39,60 @@ int col_sum(int n,int **m,int c){
     int result = 0;
     for (int i = 0; i < n; i++)
         result+=m[i][c];
+    return result;
 }
 
 int row_sum(int n, int *a){
     int result = 0;
     for (int i = 0; i < n; i++)
         result+=a[i];
+    return result;
 }
 
 int DLR_sum(int n,int **m){
     int result = 0;
     for (int i = 0; i < n; i++)
         result+=m[i][i];
+    return result;
 }
 
 int DRL_sum(int n,int **m){
     int result = 0;
     for (int i = 0; i < n; i++)
-        result+=m[i][n-(i+1)];   
+        result+=m[i][n-(i+1)];
+    return result;   
 }
 
-bool check_magic_square(int n,int **m){
-    bool b=false;
-    int col_value = 0;
-    int row_value = 0;
+bool check_rows(int n,int **m,int row_value){
+    for (int i = 1; i < n; i++)
+        if(row_value != row_sum(n,m[i]))
+            return false;
+    return true;
+}
 
-    for (int i = 0; i < n; i++)
-    {
-        int 
-        if()
-        
-    }
-    
-    return false;
+bool check_cols(int n,int **m,int col_value){
+    for (int i = 1; i < n; i++)
+        if(col_value != col_sum(n,m,0))
+            return false;
+    return true;
+}
+
+char* check_magic_square(int n,int **m){
+    int col_value = col_sum(n,m,0);
+    int row_value = row_sum(n,m[0]);
+    int dlr_value = DLR_sum(n,m);
+    int drl_value = DRL_sum(n,m);
+
+    //printf("col: %d, row %d, dlr %d, drl %d\n",col_value,row_value,dlr_value,drl_value);
+    //printf("%d %d %d\n",check_cols(n,m,col_value),check_rows(n,m,row_value));
+
+    if((col_value == row_value && row_value == dlr_value && dlr_value == drl_value) && check_cols(n,m,col_value)==1 && check_rows(n,m,row_value)==1)
+        return "Magic Square";
+    else if(!check_cols(n,m,col_value) && !check_rows(n,m,row_value) && dlr_value != drl_value)
+        return "Not a Magic Square";
+    else return "Defective Magic Square";
+
+
 }
 
 void magic_square(){
@@ -88,6 +109,8 @@ void magic_square(){
     
     to_magic_square(l,ms,m,n);
     ints_println_basic(m,n);
+
+    printf("%s",check_magic_square(l,ms));
     
     fclose(fp);
     
